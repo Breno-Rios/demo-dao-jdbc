fe95902426150849e64de464d2f6cbbbd85781b9
Breno-Rios <brenooliveirarios@gmail.com>
2024-10-19 15:40:21 -0300

    Department and Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Department department = new Department(1,"Qualidade");
+		
+		Seller seller = new Seller(1, "Breno", "Breno@gmail.com", new Date(), 2000.0, department);
+		
+		System.out.println(seller);
 
 	}
 
